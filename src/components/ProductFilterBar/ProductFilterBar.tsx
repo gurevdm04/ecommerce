633a81a20ff23fa5ce70116cd70bc4af4959c5bc
gurevdm04 <gurevdm04@gmail.com>
2024-11-07 19:21:17 +1000

    create productfilter

--- src/components/ProductFilterBar/ProductFilterBar.tsx
+++ src/components/ProductFilterBar/ProductFilterBar.tsx
@@ -1,19 +1,24 @@
 import style from "./ProductFilterBar.module.scss";
 
+import { HiAdjustmentsHorizontal } from "react-icons/hi2";
+import { HiMiniSquares2X2 } from "react-icons/hi2";
+import { HiOutlineQueueList } from "react-icons/hi2";
+
 export const ProductFilterBar = () => {
   return (
     <div className={style.wrap}>
-      <div className="container">
-        <div></div>
-        <div>
-          <div>
-            <p>Show</p>
-            <input type="number" />
-          </div>
-          <div>
-            <p>Short by</p>
-            <input type="number" />
-          </div>
+      <div className={style.container}>
+        <div className={style.setting}>
+          <HiAdjustmentsHorizontal />
+          Filter
+          <HiMiniSquares2X2 />
+          <HiOutlineQueueList />| Showing 1–16 of 32 results
+        </div>
+        <div className={style.filter}>
+          <p>Show</p>
+          <input type="text" placeholder="16" />
+          <p>Short by</p>
+          <input type="text" placeholder="Default" />
         </div>
       </div>
     </div>
