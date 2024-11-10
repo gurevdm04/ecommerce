d9a15f7b084418a33ba5510d4d4a1a1f01422d01
gurevdm04 <gurevdm04@gmail.com>
2024-11-10 14:32:13 +1000

    create routes

---
+++ src/constants/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  HOME: "/",
+  SHOP: "/shop",
+  SHOP_ITEM: (id: string | number) => `/shop/${id}`,
+  CART: "/cart",
+  CHECKOUT: "/checkout",
+  CONTACT: "/contact",
+  PROFILE: "/profile",
+};
