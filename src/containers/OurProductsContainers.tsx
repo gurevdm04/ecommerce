6196362ef39e7514742cde2966a51201b8ddc105
gurevdm04 <gurevdm04@gmail.com>
2024-11-16 19:24:54 +1000

    Добавление в OurProductsContainers вывода товаров

--- src/containers/OurProductsContainers.tsx
+++ src/containers/OurProductsContainers.tsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Products } from "../components/Products/Products";
 import { Button } from "../components/Button/Button";
+import { ProductProps } from "../components/Product/Product";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
 
 export const OurProductsContainers = () => {
-  const titleStyle: React.CSSProperties | undefined = {
-    fontWeight: 700,
-    fontSize: "40px",
-    lineHeight: "120%",
-    textAlign: "center",
-    color: "#3a3a3a",
-    margin: "56px 0 32px 0",
-  };
-  const btnStyle: React.CSSProperties | undefined = {
-    display: "block",
-    margin: "32px auto 0 auto",
-  };
+  const [products, setProducts] = useState<ProductProps[]>([]);
 
-  const items: any[] = Array.from({ length: 8 }, (_, index) => index);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const querySnapshot = await getDocs(collection(db, "products"));
+      const productsData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as ProductProps[];
+      setProducts(productsData);
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <>
       <h2 style={titleStyle}>Our Products</h2>
-      <Products products={items} />
+      <Products products={products} />
       <Button style={btnStyle} label="Show More" />
     </>
   );
+};
+
+const titleStyle: React.CSSProperties | undefined = {
+  fontWeight: 700,
+  fontSize: "40px",
+  lineHeight: "120%",
+  textAlign: "center",
+  color: "#3a3a3a",
+  margin: "56px 0 32px 0",
+};
+const btnStyle: React.CSSProperties | undefined = {
+  display: "block",
+  margin: "32px auto 0 auto",
 };
