52fcf999cba294f1959db7f7870b1e577c649d3c
gurevdm04 <gurevdm04@gmail.com>
2024-11-16 15:02:46 +1000

    completion of BreadcrumbHeader functionality

--- src/components/BreadcrumbHeader/BreadcrumbHeader.tsx
+++ src/components/BreadcrumbHeader/BreadcrumbHeader.tsx
@@ -1,15 +1,21 @@
 import style from "./BreadcrumbHeader.module.scss";
 import img from "./../../assets/images/Breadcrumb.png";
+import { useLocation } from "react-router-dom";
 
 export const BreadcrumbHeader = () => {
+  const { pathname } = useLocation();
+
+  const str = pathname.split("/")[1];
+  const breadcrumbTitle = str.charAt(0).toUpperCase() + str.slice(1);
+  
   return (
     <div className={style.wrap}>
       <img className={style.img} src={img} alt="" />
-      <h2 className={style.title}>Shop</h2>
+      <h2 className={style.title}>{breadcrumbTitle}</h2>
       <div className={style.breadcrumb}>
-        <span className={style.root}>Shop</span>
+        <span className={style.root}>Home</span>
         <span className={style.icon}>{">"}</span>
-        <span className={style.page}>Shop</span>
+        <span className={style.page}>{breadcrumbTitle}</span>
       </div>
     </div>
   );
