7a2e4cbca38fad8e5c7d686b59d51d25e5eaeec3
gurevdm04 <gurevdm04@gmail.com>
2024-11-16 17:49:55 +1000

    added styles for disabled buttons. implementation of sending feedback in telegram

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,8 +4,72 @@ import { FaPhone } from "react-icons/fa6";
 import { MdAccessTimeFilled } from "react-icons/md";
 import { Button } from "../Button/Button";
 import { Wrapper } from "../Wrapper/Wrapper";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Oval } from "react-loader-spinner";
+
+const TELEGRAM_API_URL = `https://api.telegram.org/bot${
+  import.meta.env.VITE_TELEGRAM_BOT_TOKEN
+}/sendMessage`;
+const CHAT_ID = import.meta.env.VITE_TELEGRAM_CHAT_ID;
 
 export const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      // Подготавливаем текст сообщения для Telegram
+      const message = `
+        Новый запрос с сайта:
+        - Имя: ${formData.name}
+        - Email: ${formData.email}
+        - Сообщение: ${formData.message}
+      `;
+
+      // Отправляем данные на Telegram
+      await axios.post(TELEGRAM_API_URL, {
+        chat_id: CHAT_ID,
+        text: message,
+      });
+
+      setIsSuccess(true);
+      setFormData({ name: "", email: "", message: "" });
+    } catch (err) {
+      setError("Не удалось отправить сообщение. Попробуйте позже.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsSuccess(false);
+    }, 2000); // 2 секунды
+
+    return () => clearTimeout(timer);
+  }, [isSuccess]);
+
   return (
     <Wrapper>
       <h2 className={style.title}>Get In Touch With Us</h2>
@@ -46,18 +110,59 @@ export const Contact = () => {
             </div>
           </div>
         </div>
-        <div className={style.form}>
-          <h4>Your name</h4>
-          <input type="text" />
-          <h4>Email address</h4>
-          <input type="text" />
-          <h4>Subject</h4>
-          <input type="text" />
-          <h4>Message</h4>
-          <textarea name="" id=""></textarea>
-          <br />
-          <Button label="Submit" />
-        </div>
+        <form onSubmit={handleSubmit} className={style.form}>
+          <fieldset disabled={isLoading}>
+            <div>
+              <label htmlFor="name">Имя:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label htmlFor="message">Сообщение:</label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isLoading && (
+              <Oval
+                visible={true}
+                height="40"
+                width="40"
+                color="#b88e2f"
+                ariaLabel="oval-loading"
+                wrapperClass={style.loader}
+                secondaryColor="#b88e2f"
+              />
+            )}
+
+            {isSuccess && <p>Сообщение отправлено успешно!</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <Button label="Submit" />
+          </fieldset>
+        </form>
       </div>
     </Wrapper>
   );
