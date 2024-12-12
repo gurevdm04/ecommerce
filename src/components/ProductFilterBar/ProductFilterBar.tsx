d9de166fca44667c869ebeee6af260751687db8e
gurevdm04 <gurevdm04@gmail.com>
2024-12-13 04:20:00 +1000

    we have added filters to the browser bar

--- src/components/ProductFilterBar/ProductFilterBar.tsx
+++ src/components/ProductFilterBar/ProductFilterBar.tsx
@@ -2,26 +2,62 @@ import style from "./ProductFilterBar.module.scss";
 
 import { Wrapper } from "../Wrapper/Wrapper";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+// TODO оптимизировать код
+// TODO сделать так чтоб параметры удалялист при пустых значениях
 
 export const ProductFilterBar = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+  const [priceMax, setPriceMax] = useState<string>("");
+  const [priceMin, setPriceMin] = useState<string>("");
+
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      updateSearchParams("priceMax", priceMax);
+    }, 1000);
+
+    return () => clearTimeout(delayDebounce);
+  }, [priceMax]);
+
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      updateSearchParams("priceMin", priceMin);
+    }, 1000);
+
+    return () => clearTimeout(delayDebounce);
+  }, [priceMin]);
+
+  const updateSearchParams = (paramName: string, paramValue: string) => {
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set(paramName, paramValue);
+    navigate(`?${newSearchParams.toString()}`);
+  };
 
   const sortOptions = [
     { value: "default", label: "По умолчанию" },
-    { value: "price-asc", label: "Сначала недорогие" },
-    { value: "price-desc", label: "Сначала дорогие" },
+    { value: "alphabetical_asc", label: "alphabetical_asc" },
+    { value: "alphabetical_desc", label: "alphabetical_desc" },
+    { value: "price_asc", label: "price_asc" },
+    { value: "price_desc", label: "price_desc" },
   ];
 
   const currentSort = searchParams.get("sort") || "default";
-
   const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedSort = event.target.value;
+    updateSearchParams("sort", selectedSort);
+  };
 
-    const newSearchParams = new URLSearchParams(searchParams);
-    newSearchParams.set("sort", selectedSort);
+  const categoryOptions = [
+    { value: "", label: "По умолчанию" },
+    { value: "Sofas", label: "Sofas" },
+  ];
 
-    navigate(`?${newSearchParams.toString()}`);
+  const currentCategory = searchParams.get("category") || "";
+  const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCategory = event.target.value;
+    updateSearchParams("category", selectedCategory);
   };
 
   return (
@@ -30,9 +66,39 @@ export const ProductFilterBar = () => {
         <div className={style.container}>
           <div className={style.setting}>| Showing 1–16 of 32 results</div>
           <div className={style.filter}>
-            <p>Short by</p>
+            <p>Category</p>
             <select
-              name="shortby"
+              name="category"
+              value={currentCategory}
+              onChange={handleCategoryChange}
+            >
+              {categoryOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className={style.filter}>
+            <p>Price Min</p>
+            <input
+              value={priceMin}
+              onChange={(e) => setPriceMin(e.target.value)}
+              type="number"
+            />
+          </div>
+          <div className={style.filter}>
+            <p>Price Max</p>
+            <input
+              value={priceMax}
+              onChange={(e) => setPriceMax(e.target.value)}
+              type="number"
+            />
+          </div>
+          <div className={style.filter}>
+            <p>Sort by</p>
+            <select
+              name="sortby"
               value={currentSort}
               onChange={handleSortChange}
             >
