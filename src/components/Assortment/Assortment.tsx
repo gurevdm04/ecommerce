4688a909798267f96263099022ae59a34d0cba57
gurevdm04 <gurevdm04@gmail.com>
2024-12-14 01:20:36 +1000

    adaptation to mobile devices

--- src/components/Assortment/Assortment.tsx
+++ src/components/Assortment/Assortment.tsx
@@ -1,6 +1,8 @@
 import style from "./Assortment.module.scss";
 
 import img from "./../../assets/images/assortment.jpg";
+import img2 from "./../../assets/images/assortment2.png";
+import img3 from "./../../assets/images/assortment3.png";
 import { Wrapper } from "../Wrapper/Wrapper";
 
 export const Assortment = () => {
@@ -11,17 +13,22 @@ export const Assortment = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
       <div className={style.row}>
-        <Item />
-        <Item />
-        <Item />
+        <Item img={img} label="Dining" />
+        <Item img={img2} label="Living" />
+        <Item img={img3} label="Bedroom" />
       </div>
     </Wrapper>
   );
 };
 
-const Item = () => (
+interface ItemProps {
+  label: string;
+  img: string;
+}
+
+const Item: React.FC<ItemProps> = ({ img, label }) => (
   <div className={style.item}>
-    <img className={style.img} src={img} alt="" />
-    <p className={style.text}>Dining</p>
+    <img className={style.img} src={img} alt={label} />
+    <p className={style.text}>{label}</p>
   </div>
 );
