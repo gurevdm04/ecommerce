a520afd504d5f1870a4db51701db90fbdbd7271b
gurevdm04 <gurevdm04@gmail.com>
2024-12-15 00:47:56 +1000

    adaptation to mobile devices

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,12 +4,14 @@ import logoImg from "./../../assets/images/logo.png";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { Wrapper } from "../Wrapper/Wrapper";
-import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { ROUTES } from "../../constants/routes";
 import { RegistrationModal } from "../RegistrationModal/RegistrationModal";
 import { useAppSelector } from "../../store/hooks";
 import { RootState } from "../../store/store";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { MdClose } from "react-icons/md";
 
 export const Header = () => {
   const isAuthenticated = useAppSelector(
@@ -17,6 +19,8 @@ export const Header = () => {
   );
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -25,41 +29,91 @@ export const Header = () => {
     setIsModalOpen(false);
   };
 
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location]);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    // Очистка стиля при размонтировании компонента
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isMenuOpen]);
+
   return (
     <>
       <Wrapper>
-        <div className={style.wrap}>
+        <header className={style.wrap}>
           <Link to={ROUTES.HOME} className={style.logo}>
             <img src={logoImg} alt="logo" />
           </Link>
-          <nav className={style.nav}>
-            <ul>
-              <LinkItem label="Home" to={ROUTES.HOME} />
-              <LinkItem label="Shop" to={ROUTES.SHOP} />
-              <LinkItem label="Contact" to={ROUTES.CONTACT} />
-            </ul>
-          </nav>
-          <div className={style.icons}>
-            {isAuthenticated ? (
-              <Link to={ROUTES.PROFILE}>
-                <FaRegUser />
-              </Link>
-            ) : (
-              <span onClick={openModal}>
-                <FaRegUser />
-              </span>
-            )}
-
-            <Link to={ROUTES.CART}>
-              <MdOutlineShoppingCart />
-            </Link>
+          <div className={style.wrapItem}>
+            <Nav />
           </div>
-        </div>
+          <div className={style.wrapItem}>
+            <Icons isAuthenticated={isAuthenticated} openModal={openModal} />
+          </div>
+          <div className={style.hamburger}>
+            <HiMenuAlt2 fontSize={32} onClick={() => setIsMenuOpen(true)} />
+          </div>
+        </header>
       </Wrapper>
       {isModalOpen && <RegistrationModal closeModal={closeModal} />}
+      {isMenuOpen && (
+        <div className={style.asideMenu}>
+          <div className={style.asideMenuWrap}>
+            <div
+              className={style.asideMenuClose}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <MdClose fontSize={32} />
+            </div>
+            <Nav />
+            <Icons isAuthenticated={isAuthenticated} openModal={openModal} />
+          </div>
+        </div>
+      )}
     </>
   );
 };
+
+const Nav = () => (
+  <nav className={style.nav}>
+    <ul>
+      <LinkItem label="Home" to={ROUTES.HOME} />
+      <LinkItem label="Shop" to={ROUTES.SHOP} />
+      <LinkItem label="Contact" to={ROUTES.CONTACT} />
+    </ul>
+  </nav>
+);
+
+interface IconsProps {
+  isAuthenticated: boolean;
+  openModal: () => void;
+}
+const Icons: React.FC<IconsProps> = ({ isAuthenticated, openModal }) => (
+  <div className={style.icons}>
+    {isAuthenticated ? (
+      <Link to={ROUTES.PROFILE}>
+        <FaRegUser />
+      </Link>
+    ) : (
+      <span onClick={openModal}>
+        <FaRegUser />
+      </span>
+    )}
+
+    <Link to={ROUTES.CART}>
+      <MdOutlineShoppingCart />
+    </Link>
+  </div>
+);
 
 interface LinkItem {
   to: string;
