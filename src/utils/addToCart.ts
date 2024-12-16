a21c7fe7a1d6d08a4d1f389446de5384a2f920f7
gurevdm04 <gurevdm04@gmail.com>
2024-12-17 02:26:04 +1000

    changing the logic of adding an item to the cart

--- src/utils/addToCart.ts
+++ src/utils/addToCart.ts
@@ -8,30 +8,14 @@ export const addToCart = async (userId: string, product: ItemCartData) => {
   try {
     const cartSnap = await getDoc(cartRef);
     if (cartSnap.exists()) {
-      const cartData = cartSnap.data();
-      const existingItem = cartData.items.find(
-        (item: any) => item.productId === product.productId
-      );
-
-      if (existingItem) {
-        // Если товар уже есть в корзине, увеличиваем количество
-        const updatedItems = cartData.items.map((item: any) =>
-          item.productId === product.productId
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-        await updateDoc(cartRef, { items: updatedItems });
-      } else {
-        // Если товара нет в корзине, добавляем новый
-        await updateDoc(cartRef, {
-          items: arrayUnion({ ...product, quantity: 1 }),
-        });
-      }
+      await updateDoc(cartRef, {
+        items: arrayUnion({ ...product }),
+      });
     } else {
       // Если корзины для пользователя еще нет, создаем её
       await setDoc(cartRef, {
         userId,
-        items: [{ ...product, quantity: 1 }],
+        items: [{ ...product }],
       });
     }
   } catch (error) {
