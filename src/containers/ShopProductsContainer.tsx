9cc77506773feaddfb6a79117cec1eaeb21c2388
gurevdm04 <gurevdm04@gmail.com>
2025-01-08 23:36:48 +1000

    fixing a bug where goods disappear when they are loaded

--- src/containers/ShopProductsContainer.tsx
+++ src/containers/ShopProductsContainer.tsx
@@ -30,7 +30,6 @@ export const ShopProductsContainer = () => {
 
   const fetchProducts = async (loadMore = false) => {
     setIsLoading(true);
-    setProducts([]);
     const filters = Object.fromEntries([...searchParams]);
 
     try {
