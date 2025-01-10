0299bbff4b0169cc62aa81a7e685f8b450c056a0
gurevdm04 <gurevdm04@gmail.com>
2025-01-11 01:46:21 +1000

    A plug has been made for all products

--- src/components/Profile/AllProducts/AllProducts.tsx
+++ src/components/Profile/AllProducts/AllProducts.tsx
@@ -1,9 +1,56 @@
+import { Product } from "../../Product/Product";
 import style from "./AllProducts.module.scss";
+
+const favorites = [
+  {
+    id: "test",
+    currentPrice: 12,
+    images: ["test"],
+    oldPrice: 13,
+    shortDesc: "test",
+    title: "test",
+  },
+  {
+    id: "test",
+    currentPrice: 12,
+    images: ["test"],
+    oldPrice: 13,
+    shortDesc: "test",
+    title: "test",
+  },
+  {
+    id: "test",
+    currentPrice: 12,
+    images: ["test"],
+    oldPrice: 13,
+    shortDesc: "test",
+    title: "test",
+  },
+];
 
 export const AllProducts = () => {
   return (
-    <div className={style.wrap}>
+    <div>
       <h2>Все товары</h2>
+      {favorites.length === 0 ? (
+        <p>Список избранного пуст</p>
+      ) : (
+        <div className={style.wrap}>
+          {favorites.map(
+            ({ id, currentPrice, images, oldPrice, shortDesc, title }) => (
+              <Product
+                id={id}
+                key={id}
+                currentPrice={currentPrice}
+                images={images}
+                oldPrice={oldPrice}
+                shortDesc={shortDesc}
+                title={title}
+              />
+            )
+          )}
+        </div>
+      )}
     </div>
   );
 };
